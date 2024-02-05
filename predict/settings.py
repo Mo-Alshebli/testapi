5be22f33e3aff18e4345d872a7b802418c2164e8
Mohammed_Alshebli <98805351+Mo-Alshebli@users.noreply.github.com>
2024-02-05 20:57:26 +0300

    Update settings.py

--- predict/settings.py
+++ predict/settings.py
@@ -125,3 +125,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['eyeapi.onrender.com', 'localhost', '127.0.0.1']
+
